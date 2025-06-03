442fce5e1acced733f73665e0b7d09675386560d
ke-xinn <tan.xin@graduate.utm.my>
2025-06-03 14:08:43 +0800

    Update device id

--- static/script.js
+++ static/script.js
@@ -1,10 +1,10 @@
 async function fetchData() {
-  const response = await fetch('https://vital-sign-server.onrender.com/data');
+  const response = await fetch('/data');
   const json = await response.json();
   let content = '';
   for (const id in json) {
     const d = json[id];
-    content += `<p>HR: ${d.hr}, BP_SYS: ${d.bp_sys}, BP_DIA: ${d.bp_dia}, Temp: ${d.temp}, SpO2: ${d.spo2}</p>`;
+    content += `<p><b>${id}</b> → HR: ${d.hr}, BP_SYS: ${d.bp_sys}, BP_DIA: ${d.bp_dia}, Temp: ${d.temp}, SpO2: ${d.spo2}</p>`;
   }
   document.getElementById('data').innerHTML = content;
 }
