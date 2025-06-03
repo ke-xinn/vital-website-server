4755bf2f0938e042020ca8703d478477c18822d6
ke-xinn <tan.xin@graduate.utm.my>
2025-06-03 13:34:31 +0800

    Update JSON and status

--- static/script.js
+++ static/script.js
@@ -4,7 +4,7 @@ async function fetchData() {
   let content = '';
   for (const id in json) {
     const d = json[id];
-    content += `<p><b>${id}</b> → Temp: ${d.temp}, HR: ${d.hr}, SpO2: ${d.spo2}, ECG: ${d.ecg}</p>, BP_SYS: ${d.bp_sys}</p>, BP_DIA: ${d.bp_dia}</p>`;
+    content += `<p>HR: ${d.hr}, BP_SYS: ${d.bp_sys}, BP_DIA: ${d.bp_dia}, Temp: ${d.temp}, SpO2: ${d.spo2}</p>`;
   }
   document.getElementById('data').innerHTML = content;
 }
